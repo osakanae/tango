637881255ee8c540308c9d50e8447c3ce8806537
kanae <hey324kana@gmail.com>
2024-12-12 11:42:41 +0900

    index.htmlを移動

--- backend/src/main/java/com/example/tango/Index.java
+++ backend/src/main/java/com/example/tango/Index.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class Index {
-    @RequestMapping("/")
+    @RequestMapping("/index.html")
     public String exec() {
         return "index";
     }
